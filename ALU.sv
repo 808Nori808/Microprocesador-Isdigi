7b1e3f8673f2e5f4fee6ab36f399009c522f1963
Hugo Arnau <itsepicgfx@gmail.com>
2023-11-19 19:59:44 +0100

    Comienzo ALU
    Creo que se puede usar la misma instrucción de control (en la ALU no en venus) independientemente de si es inmediato o registro, dado que a la ALU solo llegan valores como tal y en ningún momento llegan direcciones por lo tanto es indistinto.
    
    Co-Authored-By: mibavel <138608632+mibavel@users.noreply.github.com>

---
+++ ALU.sv
@@ -0,0 +1,20 @@
+module ALU (X, Y, RESULTADO, ZERO, CONTROL);
+
+input [31:0] X, Y;
+
+input [5:0] CONTROL;
+
+output [31:0] RESULTADO;
+
+output ZERO;
+
+always_comb(.*)
+begin
+case (CONTROL)
+	5'b10000: RESULTADO = X + Y;  // ADD O ADDI
+	5'b11000: RESULTADO = X < Y; // STL O STLI
+	
+	default: RESULTADO = 0; // Caso por defecto
+endcase
+end
+endmodule 
