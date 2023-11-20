cd476210aa4ee74f69a8a706f354d3e3eb1a64a7
Hugo Arnau <itsepicgfx@gmail.com>
2023-11-20 11:46:56 +0100

    Cambio de bits CONTROL

--- ALU.sv
+++ ALU.sv
@@ -2,7 +2,7 @@ module ALU (X, Y, RESULTADO, ZERO, CONTROL);
 
 input [31:0] X, Y;
 
-input [5:0] CONTROL;
+input [4:0] CONTROL;
 
 output [31:0] RESULTADO;
 
@@ -13,6 +13,7 @@ begin
 case (CONTROL)
 	5'b10000: RESULTADO = X + Y;  // ADD O ADDI
 	5'b11000: RESULTADO = X < Y; // STL O STLI
+	5'b11100: RESULTADO = X > Y; 
 	
 	default: RESULTADO = 0; // Caso por defecto
 endcase
