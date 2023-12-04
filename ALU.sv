e10c595c6051076e9afa5673c72c06757778f8cb
feguita <151412144+feguita@users.noreply.github.com>
2023-12-04 18:08:09 +0100

    Update ALU.sv

--- ALU.sv
+++ ALU.sv
@@ -16,11 +16,10 @@ assign Yu = Y;
 always_comb
 begin
 case (CONTROL)
-	4'b0000: RESULTADO = X + Y;  // ADD ADDI AUIPC // Se supone que viene ya preparado para la ALU
+	4'b0000: RESULTADO = X + Y;  // ADD ADDI AUIPC LW SW// Se supone que viene ya preparado para la ALU
 	4'b0111: RESULTADO = X - Y;  // SUB BEQ
-	4'b1111: RESULTADO = X == Y; // BNE
-	4'b0100: RESULTADO = (X < Y) ? 1 : 0; // SLT SLTI 
-	4'b1100: RESULTADO = (Xu < Yu) ? 1 : 0; // STLU STLIU
+	4'b0100: RESULTADO = (X < Y) ? 1 : 0; // SLT SLTI BLT 
+	4'b1100: RESULTADO = (Xu < Yu) ? 1 : 0; // STLU STLIU BLTU
 	4'b0010: RESULTADO = (X & Y); // AND ANDI
 	4'b0001: RESULTADO = (X | Y); // OR ORI
 	4'b1001: RESULTADO = (X ^ Y); // XOR XORI
@@ -28,10 +27,10 @@ case (CONTROL)
 	4'b1000: RESULTADO = Xu << Yu; // SLL SLLI
 	4'b1010: RESULTADO = Xu >> Yu; // SRL SRLI
 	4'b1110: RESULTADO = X >>> Y; // SRA 
+	4'b1011: RESULTADO = (X >= Y) ? 1 : 0; //BGE
 	default: RESULTADO = 0; // Caso por defecto
 endcase
-end
-
+end	
 assign ZERO = (RESULTADO == 0) ? 1 : 0;
 
 endmodule 
