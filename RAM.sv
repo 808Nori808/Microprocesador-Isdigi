1d21d39f318de3d85188c89de3d1e59668bdbdab
glace49 <beltransanzhugo@gmail.com>
2023-12-10 19:45:55 +0100

    RAM la he dejado como estaba

--- RAM.sv
+++ RAM.sv
@@ -2,11 +2,7 @@ module RAM #(parameter mem_depth=1024, parameter size=32)
 (
 input [size-1:0] data,
 input wren, wread, clock,
-//----------------------------------------
-// input [$clog2(mem_depth-1)-1:0] address, // La entrada de address viene directamente conectada de Alu Result, 
-// // entonces su tamaño es de [31:0], o sea [size-1:0] , por eso yo cambiaría la línea y pondría
-input [size-1:0] address
-// ---------------------------------------
+input [$clog2(mem_depth-1)-1:0] address
 output logic [size-1:0] salida
 );	
  
