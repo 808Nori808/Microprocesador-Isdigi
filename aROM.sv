5a43856edf5207956ebb80048282a01addf7b086
glace49 <beltransanzhugo@gmail.com>
2023-12-12 01:01:39 +0100

    YA VAAAAA

--- aROM.sv
+++ aROM.sv
@@ -6,11 +6,12 @@ parameter tam_salida = 32;
 input [$clog2(tam_entrada-1)-1:0] address;
 output [tam_salida-1:0] dsalida;
  
-logic [tam_salida-1:0] mem[(1<<tam_entrada)-1:0] ; 
+logic [tam_salida-1:0] mem[tam_entrada-1:0] ; 
+
+assign dsalida = mem[address];
  
 initial
     $readmemh("myFibo.hex", mem);
  
-assign dsalida = mem[address];
 
 endmodule 
